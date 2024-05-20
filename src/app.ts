4543144bbc40a3ee305a27e04500cf4919b64e1c
humayonitbd <humayonforid44@gmail.com>
2024-05-20 20:57:52 +0600

    project setup and folder stucture and create route,interface,model,controller,service

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,14 @@
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
-import { StudentRoute } from './app/modules/student/student.route';
+import { StudentRoute } from './app/modules/product/product.route';
 const app: Application = express();
 
 //parsers
 app.use(express.json());
-
 app.use(cors());
 
 // application route..
-app.use('/api/v1/students', StudentRoute);
+app.use('/api/products', StudentRoute);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('server is running.');
---
+++ src/app/modules/product/product.model.ts
@@ -0,0 +1,28 @@
+import { Schema, model } from 'mongoose';
+import { TInventory, TProduct, TVariant } from './product.interface';
+
+// Mongoose schemas
+const VariantSchema = new Schema<TVariant>({
+  type: { type: String, required: true },
+  value: { type: String, required: true },
+});
+
+const InventorySchema = new Schema<TInventory>({
+  quantity: { type: Number, required: true },
+  inStock: { type: Boolean, required: true },
+});
+
+const ProductSchema = new Schema<TProduct>({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  category: { type: String, required: true },
+  tags: { type: [String], required: true },
+  variants: { type: [VariantSchema], required: true },
+  inventory: { type: InventorySchema, required: true },
+});
+
+// Mongoose model
+const Product = model<TProduct>('Product', ProductSchema);
+
+export default Product;
